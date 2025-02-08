494d6fdf58ee9562abcd0771f54e8e1cd785e90e
Athiwat <scboyhz@gmail.com>
2025-02-08 19:35:43 +0700

    implement qstash workflows for user onboarding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,11 +2,34 @@ import { auth } from "@/auth";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
+import { after } from "next/server";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get user and see if the last activity date is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toDateString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
 
   return (
     <main className="root-container">
